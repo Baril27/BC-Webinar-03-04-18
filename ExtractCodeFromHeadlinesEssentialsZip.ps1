0fa8d9c8d3c79cb5f15beaf6e9b59cc52f57ef20
Dmitry Katson <dmitry.katson@gmail.com>
2018-04-15 16:17:10 +0000

    headline table

--- ExtractCodeFromHeadlinesEssentialsZip.ps1
+++ ExtractCodeFromHeadlinesEssentialsZip.ps1
@@ -4,6 +4,7 @@ docker ps
 
 Install-Module -Name navconteinerhelper -Force
 
+#1. Exploring NavContainer files
 Enter-NavContainer navserver
 
 cd C:\
@@ -12,6 +13,7 @@ cd C:\extensions
 dir
 Exit
 
+#2. Copy file with Extension source code from Container to host
 docker cp navserver:C:\extensions\EssentialBusinessHeadlines.Source.zip C:\ProgramData\navcontainerhelper\Extensions\navserver\my
 Invoke-Item C:\ProgramData\navcontainerhelper\Extensions\navserver\my
 
@@ -19,14 +21,18 @@ Copy-Item C:\ProgramData\navcontainerhelper\Extensions\navserver\my\EssentialBus
 
 Invoke-Item C:\Users\vmadmin\Documents\AL
 
-#Extract zip
+#3. Extract zip
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 [System.IO.Compression.ZipFile]::ExtractToDirectory('C:\Users\vmadmin\Documents\AL\EssentialBusinessHeadlines.Source.zip', 'C:\Users\vmadmin\Documents\AL\EssentialBusinessHeadlines.Source')
 Invoke-Item C:\Users\vmadmin\Documents\AL
 
-#Open VSCode
+#4. Open VSCode
+
+#5. Show table with headlines results
+Start 'https://air-d365bc-dev.westeurope.cloudapp.azure.com/nav/?tenant=default&signInRedirected=1&table=1438'
+
+#6. Remove old files
 Remove-Item C:\ProgramData\navcontainerhelper\Extensions\navserver\my\EssentialBusinessHeadlines.Source.zip
 Remove-Item C:\Users\vmadmin\Documents\AL\EssentialBusinessHeadlines.Source.zip -Force
 Remove-Item C:\Users\vmadmin\Documents\AL\EssentialBusinessHeadlines.Source -Force
-
 
