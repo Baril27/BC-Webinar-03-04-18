57def2acbdc9115c8f1628c3a570bf6403250d99
Dmitry Katson <dmitry.katson@gmail.com>
2018-04-15 09:27:54 +0000

    sandbox script

---
+++ CreateSandBoxEnvironment.ps1
@@ -0,0 +1,16 @@
+#1. Open production environment
+Start 'https://businesscentral.dynamics.com/'
+
+#trial e-mail : 2ilham.fasi@hdprice.co
+#trial password: Password12345$
+
+#2. Create Sandbox Environment - online
+
+#3. Create Sandbox Environment - locally 
+# need to 
+# download CreateBCSandbox.ps1 script
+# change $accept_eula = $true
+# change $containername = 'Your container name'
+# change $navdockerimage = 'microsoft/bcsandbox:12.0.21229.0-us'
+# run script
+
--- OpenSandBoxEnvironment.ps1
+++
@@ -1,7 +0,0 @@
-#1. Open production environment
-Start 'https://businesscentral.dynamics.com/'
-
-#trial e-mail : 2ilham.fasi@hdprice.co
-#trial password: Password12345$
-
-#2. Create Sandbox Environment
