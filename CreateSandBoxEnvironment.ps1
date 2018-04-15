f2bc17b6fff7ebf87e3bd3cfccc52967c00ccfc7
Dmitry Katson <dmitry.katson@gmail.com>
2018-04-15 15:31:11 +0000

    sandbox

--- CreateSandBoxEnvironment.ps1
+++ CreateSandBoxEnvironment.ps1
@@ -5,12 +5,17 @@ Start 'https://businesscentral.dynamics.com/'
 #trial password: Password12345$
 
 #2. Create Sandbox Environment - online
+Start 'https://businesscentral.dynamics.com/sandbox'
 
 #3. Create Sandbox Environment - locally 
 # need to 
 # download CreateBCSandbox.ps1 script
 # change $accept_eula = $true
-# change $containername = 'Your container name'
-# change $navdockerimage = 'microsoft/bcsandbox:12.0.21229.0-us'
+# change $containername = 'Your container name' (ex. airapps-sandbox)
+# change $navdockerimage = 'microsoft/bcsandbox:12.0.21229.0-us' (bug)
 # run script
+Start 'http://airapps-sandbox/nav/'
 
+#Links
+#docker images
+Start 'https://store.docker.com/community/images/microsoft/bcsandbox/tags'
